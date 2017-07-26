def4c9fbe3839a43e5255b4edab12e8b66e477ea
Unknown <alejandro.veronesi@hotmail.com>
2017-07-26 10:33:46 -0300

    Con autenticacion de twitter y dolores de cabeza con la google

--- app/src/main/java/com/example/alejandroveronesi/omicron742/LoginActivity.java
+++ app/src/main/java/com/example/alejandroveronesi/omicron742/LoginActivity.java
@@ -1,21 +1,66 @@
 package com.example.alejandroveronesi.omicron742;
 
+import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.tweetcomposer.TweetComposer;
+import com.twitter.sdk.android.tweetui.TweetUi;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button twitterButton;
     Button facebookButton;
+    TwitterLoginButton loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        TwitterCore.getInstance();
+        TweetUi.getInstance();
+        TweetComposer.getInstance();
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // Do something with result, which provides a TwitterSession for making API calls
+                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // Do something on failure
+            }
+        });
+
+
+    }
 
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result to the login button.
+        loginButton.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ app/src/main/java/com/example/alejandroveronesi/omicron742/MyResultReceiverTwitter.java
@@ -0,0 +1,28 @@
+package com.example.alejandroveronesi.omicron742;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.twitter.sdk.android.tweetcomposer.TweetUploadService;
+
+/**
+ * Created by alejandroveronesi on 26/7/17.
+ */
+
+public class MyResultReceiverTwitter extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (TweetUploadService.UPLOAD_SUCCESS.equals(intent.getAction())) {
+            // success
+            Toast.makeText(context, "OK", Toast.LENGTH_SHORT).show();
+            final Long tweetId = intent.getExtras().getLong(TweetUploadService.EXTRA_TWEET_ID);
+        } else {
+            // failure
+            Toast.makeText(context, "NO Ok", Toast.LENGTH_SHORT).show();
+            final Intent retryIntent = intent.getExtras().getParcelable(TweetUploadService.EXTRA_RETRY_INTENT);
+        }
+    }
+}
+
