c5966878c12caffe64b089c6859a61580ed4be1d
Federico Dassatti <srbfdc@gmail.com>
2017-07-26 12:42:13 -0300

    sin novedad

--- app/src/main/java/com/example/alejandroveronesi/omicron742/View/Fragments/FragmentEvent.java
+++ app/src/main/java/com/example/alejandroveronesi/omicron742/View/Fragments/FragmentEvent.java
@@ -17,7 +17,9 @@ public class FragmentEvent extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event, container, false);
+        View view =  inflater.inflate(R.layout.fragment_event, container, false);
+
+        return view;
     }
 
 }
--- app/src/main/java/com/example/alejandroveronesi/omicron742/View/Fragments/FragmentEventManager.java
+++ app/src/main/java/com/example/alejandroveronesi/omicron742/View/Fragments/FragmentEventManager.java
@@ -1,6 +1,5 @@
 package com.example.alejandroveronesi.omicron742.View.Fragments;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,17 +8,15 @@ import android.view.ViewGroup;
 
 import com.example.alejandroveronesi.omicron742.R;
 
-
 public class FragmentEventManager extends Fragment {
-
-    
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_manager, container, false);
+        View view = inflater.inflate(R.layout.fragment_event_manager, container, false);
+
+        return view;
     }
 
 }
