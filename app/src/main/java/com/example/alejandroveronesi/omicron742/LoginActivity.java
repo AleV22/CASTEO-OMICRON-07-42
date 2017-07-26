c339575f09dddb245e8f845ea88244c080c46205
Unknown <alejandro.veronesi@hotmail.com>
2017-07-25 21:37:43 -0300

    Nada importante

--- app/src/main/java/com/example/alejandroveronesi/omicron742/LoginActivity.java
+++ app/src/main/java/com/example/alejandroveronesi/omicron742/LoginActivity.java
@@ -2,12 +2,20 @@ package com.example.alejandroveronesi.omicron742;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button twitterButton;
+    Button facebookButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
+
     }
 }
