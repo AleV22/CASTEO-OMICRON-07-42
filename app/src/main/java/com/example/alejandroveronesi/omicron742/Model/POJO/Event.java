d6a64b038932b8be2e23fd5ca3ff2a5efb0a2670
Federico Dassatti <srbfdc@gmail.com>
2017-07-26 10:15:05 -0300

    Primeros bolasos

---
+++ app/src/main/java/com/example/alejandroveronesi/omicron742/Model/POJO/Event.java
@@ -0,0 +1,15 @@
+package com.example.alejandroveronesi.omicron742.Model.POJO;
+
+
+
+public class Event {
+
+    private String eventName;
+    private Integer time;
+    private String contact;
+    private String position;
+
+    
+
+
+}
