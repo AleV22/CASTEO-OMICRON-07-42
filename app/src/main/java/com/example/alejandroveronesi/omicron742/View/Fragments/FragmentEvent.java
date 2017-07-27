188ccdad9523fc839a9eb17864ede4bdaefaf812
Federico Dassatti <srbfdc@gmail.com>
2017-07-27 17:21:18 -0300

    con selector de minutos, en construccion

--- app/src/main/java/com/example/alejandroveronesi/omicron742/View/Fragments/FragmentEvent.java
+++ app/src/main/java/com/example/alejandroveronesi/omicron742/View/Fragments/FragmentEvent.java
@@ -1,11 +1,14 @@
 package com.example.alejandroveronesi.omicron742.View.Fragments;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import com.example.alejandroveronesi.omicron742.Model.POJO.Event;
 import com.example.alejandroveronesi.omicron742.R;
@@ -22,6 +25,34 @@ public class FragmentEvent extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_event, container, false);
+
+        //Get the widgets reference from XML layout
+        final TextView tv = (TextView) view.findViewById(R.id.tv);
+        NumberPicker np = (NumberPicker) view.findViewById(R.id.np);
+
+        //Set TextView text color
+        tv.setTextColor(Color.parseColor("#ffd32b3b"));
+
+        //Populate NumberPicker values from minimum and maximum value range
+        //Set the minimum value of NumberPicker
+        np.setMinValue(0);
+        //Specify the maximum value/number of NumberPicker
+        np.setMaxValue(59);
+
+        //Gets whether the selector wheel wraps when reaching the min/max value.
+        np.setWrapSelectorWheel(true);
+
+        //Set a value change listener for NumberPicker
+        np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal){
+                //Display the newly selected number from picker
+                tv.setText("Tiempo limite: " + newVal);
+            }
+        });
+
+
+
 
         return view;
     }
