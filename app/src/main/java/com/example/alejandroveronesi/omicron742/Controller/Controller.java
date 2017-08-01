90adcd20324dbc12eff8c9093847bea5a5b2acde
Federico Dassatti <srbfdc@gmail.com>
2017-08-01 13:12:19 -0300

    otros cambios Dao y Controller

--- app/src/main/java/com/example/alejandroveronesi/omicron742/Controller/Controller.java
+++ app/src/main/java/com/example/alejandroveronesi/omicron742/Controller/Controller.java
@@ -1,6 +1,25 @@
 package com.example.alejandroveronesi.omicron742.Controller;
 
 
+import com.example.alejandroveronesi.omicron742.Model.DAO.DAOEvents;
+import com.example.alejandroveronesi.omicron742.Model.POJO.Event;
+import com.example.alejandroveronesi.omicron742.util.ResultListener;
+
+import java.util.List;
 
 public class Controller {
+
+    public void getListPaints(final ResultListener<List<Event>> viewListener){
+        DAOEvents daoEvents = new DAOEvents();
+        daoEvents.obtainEvents(new ResultListener<List<Event>>() {
+            @Override
+            public void finish(List<Event> resultado) {
+                if (resultado != null){
+                    viewListener.finish(resultado);
+                }
+            }
+        });
+
+
+    }
 }
