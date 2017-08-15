93953248a40691fac6563e1653b86aa70f94cb82
Unknown <alejandro.veronesi@hotmail.com>
2017-08-15 18:41:52 -0300

    Con permisos, y casi funcionando, cambiado de logica de pedido, que la fuerza nos acompañe, JEBUS!

--- app/src/main/java/com/example/alejandroveronesi/omicron742/View/Fragments/FragmentStartEvent.java
+++ app/src/main/java/com/example/alejandroveronesi/omicron742/View/Fragments/FragmentStartEvent.java
@@ -1,17 +1,22 @@
 package com.example.alejandroveronesi.omicron742.View.Fragments;
 
 
+import android.Manifest;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.telephony.SmsManager;
 import android.view.LayoutInflater;
@@ -31,7 +36,7 @@ import java.util.concurrent.TimeUnit;
 import static android.R.attr.phoneNumber;
 
 
-public class FragmentStartEvent extends Fragment implements LocationListener {
+public class FragmentStartEvent extends Fragment {
 
     public static final String EVENT_NAME = "name";
     public static final String EVENT_TIME = "time";
@@ -41,8 +46,7 @@ public class FragmentStartEvent extends Fragment implements LocationListener {
     private Button buttonStart;
     private Location currentLocation;
     private LocationManager locationManager;
-    private Button btnObtener;
-    private TextView tvCoordenates;
+    private LocationListener locationListener;
     Double latitud;
     Double longitud;
 
@@ -52,42 +56,63 @@ public class FragmentStartEvent extends Fragment implements LocationListener {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_start_event, container, false);
 
-        //currentLocation = new Location("http://maps.google.com?q=");
+        locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitud = location.getLatitude();
+                longitud = location.getLongitude();
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        configure_button();
+
 
         Bundle bundle = getArguments();
 
         String name = bundle.getString(EVENT_NAME);
-        final Integer time = bundle.getInt(EVENT_TIME)*60000;
+        final Integer time = bundle.getInt(EVENT_TIME) * 60000;
         String email = bundle.getString(EVENT_EMAIL);
         Integer phone = bundle.getInt(EVENT_PHONE);
 
-        TextView nameEvent =  view.findViewById(R.id.nameEventTextView);
-        TextView emailEvent =  view.findViewById(R.id.emailEventTextView);
-        TextView phoneEvent =  view.findViewById(R.id.phoneEventTextView);
+        TextView nameEvent = view.findViewById(R.id.nameEventTextView);
+        TextView emailEvent = view.findViewById(R.id.emailEventTextView);
+        TextView phoneEvent = view.findViewById(R.id.phoneEventTextView);
         final TextView timeEvent = view.findViewById(R.id.timerTextView);
 
         nameEvent.setText(name);
         emailEvent.setText(email);
         phoneEvent.setText(phone.toString());
-        timeEvent.setText(String.format(Locale.getDefault(),"%d min, %d sec",
-                TimeUnit.MILLISECONDS.toMinutes( time),
+        timeEvent.setText(String.format(Locale.getDefault(), "%d min, %d sec",
+                TimeUnit.MILLISECONDS.toMinutes(time),
                 TimeUnit.MILLISECONDS.toSeconds(time) -
                         TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(time))));
 
-        tvCoordenates = (TextView)view.findViewById(R.id.textViewCoordenates);
-        btnObtener = (Button)view.findViewById(R.id.btnObtener);
-        btnObtener.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getLocation();
-            }
-        });
 
         buttonStart = view.findViewById(R.id.startButton);
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getLocation();
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, locationListener);
                 new CountDownTimer(time, 1000) { // adjust the milli seconds here
 
                     public void onTick(long millisUntilFinished) {
@@ -108,8 +133,8 @@ public class FragmentStartEvent extends Fragment implements LocationListener {
 //                        SmsManager smsManager = SmsManager.getDefault();
 //                        smsManager.sendTextMessage("+5491132870691", null, "Auxilio", null, null);
 
-                          sendLocationSMS("+5491132870691", currentLocation);
-//                          Toast.makeText(getContext(),"Current Location:" + currentLocation, Toast.LENGTH_LONG).show();
+                          //sendLocationSMS("+5491132870691", currentLocation);
+                          Toast.makeText(getContext(),"Current Location:" + currentLocation + longitud + latitud, Toast.LENGTH_LONG).show();
 
                     }
                 }.start();
@@ -129,67 +154,31 @@ public class FragmentStartEvent extends Fragment implements LocationListener {
         smsManager.sendTextMessage(phoneNumber, null, smsBody.toString(), null, null);
     }
 
-
-    void getLocation() {
-        try {
-            locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
-//            LocationListener locationListener = new LocationListener();
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                configure_button();
+                break;
+            default:
+                break;
         }
-        catch(SecurityException e) {
-            e.printStackTrace();
+    }
+
+    void configure_button() {
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
+                        , 10);
+            }
+            return;
         }
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        longitud = location.getLongitude();
-        latitud = location.getLatitude();
 
     }
 
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-        @Override
-        public void onLocationChanged(Location location) {
-            latitud = location.getLatitude();
-            longitud = location.getLongitude();
-            tvCoordenates.setText("Current Location:" + location.getLongitude() + location.getLatitude());
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
-
-
-//    private class LocationListener implements android.location.LocationListener{
-//
-//        @Override
-//        public void onLocationChanged(Location location) {
-//            latitud = location.getLatitude();
-//            longitud = location.getLongitude();
-//            tvCoordenates.setText("Current Location:" + location.getLongitude() + location.getLatitude());
-//        }
-//        @Override
-//        public void onStatusChanged(String s, int i, Bundle bundle) {
-//
-//        }
-//
-//        @Override
-//        public void onProviderEnabled(String s) {
-//            Toast.makeText(getContext(), "s", Toast.LENGTH_SHORT).show();
-//        }
-//
-//        @Override
-//        public void onProviderDisabled(String s) {
-//
-//        }
-//    }
 }
